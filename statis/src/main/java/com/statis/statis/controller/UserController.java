f38d97adee986593dc0a9e196278dac5bbb6c315
pandexiong <pandexiong@chances.com.cn>
2019-07-01 21:30:52 +0800

    登录功能

--- statis/src/main/java/com/statis/statis/controller/UserController.java
+++ statis/src/main/java/com/statis/statis/controller/UserController.java
@@ -3,11 +3,14 @@ package com.statis.statis.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.statis.statis.model.User;
 import com.statis.statis.service.UserService;
-
+@CrossOrigin(origins = "*", maxAge = 3600) 
 @RestController
 public class UserController {
   @Autowired
@@ -23,4 +26,8 @@ public class UserController {
 	 return userService.getD2Users();
  }
   
+  @RequestMapping(value="/login",method = RequestMethod.POST)
+  public Object geUser(@RequestBody User user){
+ 	 return  userService.getUser(user);
+  }
 }
--- statis/src/main/java/com/statis/statis/mapper/db/UserMapper.java
+++ statis/src/main/java/com/statis/statis/mapper/db/UserMapper.java
@@ -3,10 +3,19 @@ package com.statis.statis.mapper.db;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.statis.statis.model.User;
 
 @Mapper
 public interface UserMapper {
   public List<User> selectUsers();
+  
+  @Select("<script>"
+		  +" select id,name,username,password,avater"
+		  +" from user u"
+		  +" where u.username=#{user.username}"
+		 +"</script>")
+  public User selectUser(@Param("user") User user);
 }
